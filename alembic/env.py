127e2417fab51193eeb22bfaa81af481d824c023
panelrail4 <panelrail4@atomicmail.io>
2026-08-10 18:53:08 +0330

    Add files via upload

---
+++ alembic/env.py
@@ -0,0 +1,12 @@
+from alembic import context
+from sqlalchemy import engine_from_config, pool
+from backend.app.database import Base
+from backend.app.models import *
+config=context.config
+target_metadata=Base.metadata
+def run_migrations_online():
+    connectable=engine_from_config(config.get_section(config.config_ini_section), prefix="sqlalchemy.", poolclass=pool.NullPool)
+    with connectable.connect() as connection:
+        context.configure(connection=connection,target_metadata=target_metadata)
+        with context.begin_transaction(): context.run_migrations()
+run_migrations_online()
